ed64118e9b613e81380b0b072d0e5d8c6a74b619
Karan Dhareshwar <karandh@jhu.edu>
2020-03-25 12:51:43 -0700

    First proto build complete

---
+++ src/key_manager/key_generator.rs
@@ -0,0 +1,3 @@
+pub fn generate_account_key() {
+	println!("Account key generated");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use casperwallet::key_manager::key_generator;
+
+
 fn main() {
     println!("Hello, world!");
+    key_generator::generate_account_key();
 }
